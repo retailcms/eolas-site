5d9b41af981292005ecdfc2cad05873c751cc0de
thejamespower <jamespower_@hotmail.co.uk>
2015-02-18 13:44:28 +0000

    General fixes/tidy

--- dist/plugins/main.js
+++ dist/plugins/main.js
@@ -1,67 +1,116 @@
 $(function() {
-    // Init Controller
+
+    var scroller = new TimelineMax();
+
+    scroller.to('.scroll', .7, {
+        y: -50,
+        repeat: -1,
+        yoyo: true,
+        ease: Power2.easeOut
+    });
+
+
     var scrollMagicController = new ScrollMagic();
     if (ScrollMagic) {
         TweenMax.set('section img, header img', {
             x: '300%'
         });
     };
-    // Create Animation for 0.5s
+
+
     var tween1 = TweenMax.to('header img', 1, {
-        // rotation: 360,
-        // transformOrigin: "right top",
         x: '0%',
         clearProps: 'transform'
-        // scale: 10
-        // ease: Power1.easeOut
     });
-    // Create the Scene and trigger when visible with ScrollMagic
     var scene1 = new ScrollScene({
         triggerElement: 'header'
-        // offset: '10%'
     }).setTween(tween1).addTo(scrollMagicController);
 
-    var tween2 = TweenMax.to('#design img', 1, {
-        // rotation: 360,
-        // transformOrigin: "right top",
-        x: '0%',
-        clearProps: 'transform'
-        // scale: 10
-        // ease: Power1.easeOut
-    });
-    // Create the Scene and trigger when visible with ScrollMagic
+    var tween2 = new TimelineMax();
+    tween2.to('#design img', 1, {
+            x: '0%',
+            clearProps: 'transform'
+        }, '-=1')
+        .to('.scroll', .7, {
+            y: -300,
+            opacity: 0,
+            onReverseComplete: scroller
+        }, '-=1')
+        .to('header img', 1, {
+            x: '-300%'
+        });
     var scene2 = new ScrollScene({
         triggerElement: '#design'
-        // offset: '10%'
     }).setTween(tween2).addTo(scrollMagicController);
 
-    var tween3 = TweenMax.to('#distribute img', 1, {
-        // rotation: 360,
-        // transformOrigin: "right top",
-        x: '0%',
-        clearProps: 'transform'
-        // scale: 10
-        // ease: Power1.easeOut
-    });
-    // Create the Scene and trigger when visible with ScrollMagic
+
+    var tween3 = new TimelineMax();
+    tween3.to('#distribute img', 1, {
+            x: '0%',
+            clearProps: 'transform'
+        })
+        .to('#design img', 1, {
+            x: '-300%'
+        });
     var scene3 = new ScrollScene({
         triggerElement: '#distribute'
-        // offset: '10%'
     }).setTween(tween3).addTo(scrollMagicController);
 
-    var tween4 = TweenMax.to('#collect img', 1, {
-        // rotation: 360,
-        // transformOrigin: "right top",
+
+    var tween4 = new TimelineMax();
+    tween4.to('#collect img', 1, {
         x: '0%',
         clearProps: 'transform'
-        // scale: 10
-        // ease: Power1.easeOut
+    }).to('#distribute img', 1, {
+        x: '-300%'
     });
-    // Create the Scene and trigger when visible with ScrollMagic
     var scene4 = new ScrollScene({
         triggerElement: '#collect'
-        // offset: '10%'
     }).setTween(tween4).addTo(scrollMagicController);
 
+
+    $('.scrollUp').click(function() {
+
+        TweenMax.to(window, 1, {
+            scrollTo: {
+                y: 0,
+                x: 0
+            },
+            ease: Power2.easeOut
+        });
+    });
+    var svg = d3.select("svg.gradLine");
+    svg.append("linearGradient")
+        .attr("id", "gradient")
+        .attr("gradientUnits", "userSpaceOnUse")
+        .attr("x1", 1.5).attr("y1", 1.5)
+        .attr("x2", 1.5).attr("y2", 692.5)
+        .selectAll("stop")
+        .data([{
+            offset: "0%",
+            color: "red"
+        }, {
+            offset: "40%",
+            color: "red"
+        }, {
+            offset: "40%",
+            color: "black"
+        }, {
+            offset: "62%",
+            color: "black"
+        }, {
+            offset: "62%",
+            color: "lawngreen"
+        }, {
+            offset: "100%",
+            color: "lawngreen"
+        }])
+        .enter().append("stop")
+        .attr("offset", function(d) {
+            return d.offset;
+        })
+        .attr("stop-color", function(d) {
+            return d.color;
+        });
 
 });
